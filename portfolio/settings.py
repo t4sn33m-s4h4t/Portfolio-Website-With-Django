88e55680e5e16b7e5c6c1673226af9b8bb51e204
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2023-07-08 13:24:40 +0600

    hi

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb', 
+        'USER': 'default', 
+        'PASSWORD': 'KV5Jp7jlxuhW',
+        'HOST': 'ep-green-bar-473348-pooler.us-east-1.postgres.vercel-storage.com', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
